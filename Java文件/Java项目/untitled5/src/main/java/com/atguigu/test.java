e767df0bedac141c4f9eb698e7d40557a8614ef1
huxinyuan <296136161@qq.com>
2022-10-18 10:41:00 +0800

    second commit

--- Java文件/Java项目/untitled5/src/main/java/com/atguigu/test.java
+++ Java文件/Java项目/untitled5/src/main/java/com/atguigu/test.java
@@ -3,6 +3,6 @@ package com.atguigu;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git 喜欢唐佳佳");
     }
 }
